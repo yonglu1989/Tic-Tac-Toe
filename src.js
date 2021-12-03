2d1415de0513dea4f8107a448b24d89fa6d366d1
Yonglu <yonglu777@gmail.com>
2021-12-03 13:21:24 -0800

    Initial logic implemented and basic layout complete.

--- src.js
+++ src.js
@@ -1,3 +1,4 @@
+'use strict';
 // Initializations 
 // GameBoard state tracking array.
 let gameBoard = [
@@ -17,6 +18,7 @@ function Player(name, symbol) {
 
 // DisplayController Module. Controls what's displayed on the board.
 const DisplayController = (() => {
+    let announcement = document.querySelector('.announcements');
     let squares = document.querySelectorAll('.square');
     let pressedSquare = "";
     squares.forEach((square) => {
@@ -25,40 +27,126 @@ const DisplayController = (() => {
         });
     });
 
-    const getSquare = () => pressedSquare;
-    const setSquare = (square, symbol) => {
-        square.innerHTML = symbol;
-    }
+    // const getSquare = () => pressedSquare;
+    // const setSquare = (square, symbol) => {
+    //     square.innerHTML = symbol;
+    // }
     
-    return {getSquare, setSquare};
+    // Updates Display based on gameboard state.
+    const updateDisplay = () => {
+        let k = 0;
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard[i].length; j++) {
+                squares[k].innerHTML = gameBoard[i][j];
+                k++;
+            }
+        }
+    }
+
+    const announce = (msg) => {
+        announcement.innerHTML = msg;
+    }
+    return {updateDisplay, announce};
 
 })();
 
-// let player1 = Player("Jim", "X");
-// let currentSquare = DisplayController.getSquare();
-// DisplayController.setSquare(currentSquare, player1.getSymbol);
 
 // Main logic controller of the game. Includes current turn and players.
 const Game = (() => {
+
+    // Game start initializations
     let player1 = Player("Jim", "X");
     let player2 = Player("Steven", "O");
-
     let currentPlayerTurn = 1;
     let currentPlayer = player1;
+    let gameOver = false;
 
     // This function activates when a player clicks a square.
     // Sets clicked square
     const playerClicked = (square) => {
-        if (currentPlayerTurn == 1) {
-            currentPlayerTurn = 0;
-            currentPlayer = player1;
+        if (!gameOver) {
+            let setSquareStatus = setSquare(square, currentPlayer.getSymbol());
+            let gameStatus = checkStatus();
+            DisplayController.updateDisplay();
+            if (setSquareStatus == "success" && gameStatus == "none") {
+                nextPlayer();
+            }
+            else if (gameStatus == "draw") {
+                DisplayController.announce("Draw!");
+                gameOver = true;
+            }
+            else if (gameStatus == player1.getSymbol()) {
+                DisplayController.announce("Congratulations! " + player1.getName() + " has won!");
+                gameOver = true;
+            }
+            else if (gameStatus == player2.getSymbol()) {
+                DisplayController.announce("Congratulations! " + player2.getName() + " has won!");
+                gameOver = true;
+            }
+        }
+    } 
+
+    const setSquare = (square, symbol) => {
+        let id = square.dataset.id;
+        let i = parseInt(id[0]);
+        let j = parseInt(id[1]);
+        if (gameBoard[i][j] == "") {
+            gameBoard[i][j] = symbol;
+            return "success";
         }
         else {
-            currentPlayerTurn = 1;
+            return "failed";
+        }
+    }
+
+    const nextPlayer = () => {
+        if (currentPlayer == player1) {
             currentPlayer = player2;
         }
-        DisplayController.setSquare(square, currentPlayer.getSymbol());
-    } 
+        else {
+            currentPlayer = player1;
+        }
+    }
+
+    // Checks the game Status. 
+    // First: check to see if it's a tie from a full board then check for victory conditions.
+    // Returns "none", symbol of victorious player, or "draw".
+    const checkStatus = () => {
+        let filled = true;
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard[i].length; j++) {
+                if (gameBoard[i][j] == "") {
+                    filled = false;
+                    break;
+                }
+            }
+        }
+
+        return (gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][0] != "") ? gameBoard[0][0]
+           :(gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][0] != "") ? gameBoard[1][0]
+           :(gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[1][0] != "") ? gameBoard[2][0]
+           :(gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[0][0] != "") ? gameBoard[0][0]
+           :(gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1] && gameBoard[0][1] != "") ? gameBoard[0][1]
+           :(gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2] && gameBoard[0][2] != "") ? gameBoard[0][2]
+           :(gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][0] != "") ? gameBoard[0][0]
+           :(gameBoard[2][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[0][2] && gameBoard[2][0] != "") ? gameBoard[2][0]
+           :filled ? "draw"
+           :"none"
+        ;
+    }
+
+    // First: reset gameBoard state to all empty.
+    // Second: Update DOM display.
+    const resetGame = () => {
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = "";
+            }
+        }
+        DisplayController.updateDisplay();
+    }
+
+
 
     return {playerClicked};
 })();
